08be55de732477a10f2d678a034c9020f50b4fa2
Masen Matthews and Nicko Dela Cruz <masen+nicko@>
2018-04-09 09:05:09 -0700

    add initial components for forum functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NewForumPostComponent } from './new-forum-post/new-forum-post.component';
+import { AddPostComponent } from './add-post/add-post.component';
+import { PostDetailComponent } from './post-detail/post-detail.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { RemovePostComponent } from './remove-post/remove-post.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NewForumPostComponent
+    NewForumPostComponent,
+    AddPostComponent,
+    PostDetailComponent,
+    PostListComponent,
+    RemovePostComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/remove-post/remove-post.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-remove-post',
+  templateUrl: './remove-post.component.html',
+  styleUrls: ['./remove-post.component.css']
+})
+export class RemovePostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
