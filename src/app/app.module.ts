6e0b9adf6946df617fbee990ec372bce81119d78
Chris <cmanswanner@gmail.com>
2018-04-09 09:22:54 -0700

    add chat component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NewForumPostComponent } from './new-forum-post/new-forum-post.component';
+import { ChatComponent } from './chat/chat.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NewForumPostComponent
+    NewForumPostComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule
